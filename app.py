26b591354cec46784eeea3b7c3c881ad757014f0
Bruno Henses <henses94@gmail.com>
2025-07-23 12:44:19 +0100

    Criação do Menu de Opções

---
+++ app.py
@@ -0,0 +1,10 @@
+def exibir_menu() -> None:
+#    Exibe o Menu de Opções:
+    print("\n" + "=" * 50)
+    print("CALCULADOR DE MULTAS POR EXCESSO DE VELOCIDADE")
+    print("=" * 50)
+    print ("1 - Localidade")
+    print ("2 - Fora da Localidade")
+    print ("3 - Autoestrada")
+    print ("4 - sair")
+    print("=" * 50)
